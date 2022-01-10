830ddb070b0708154c091295d12ba9d094b64606
Valério Pires <valeriotcu007@hotmail.com>
2022-01-10 20:49:24 -0300

    Projeto inicial para controlar fila de atendimento usando estrutura de dados.

---
+++ FilaAtendimento/FilaAtendimento/Fila.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FilaAtendimento
+{
+    public class Fila
+    {
+        private Queue<int> _filaDeAtendimento;
+        private int _proximoDaFila = 1;
+
+        public Fila()
+        {
+            _filaDeAtendimento = new Queue<int>();
+        }
+
+        public int GerarSenha()
+        {
+            _filaDeAtendimento.Enqueue(_proximoDaFila);
+            Console.WriteLine("Senha gerada com sucesso!");
+            return _proximoDaFila++;
+        }
+
+        public void ChamarProximo()
+        {
+            var proximoNumeroDaFila = _filaDeAtendimento.Dequeue();
+            Console.WriteLine($"Próxima senha: {proximoNumeroDaFila}");
+        }
+
+        public void MostrarFila()
+        {
+            Console.Write("Senhas da Fila: ");
+
+            foreach (var senha in _filaDeAtendimento)
+            {
+                Console.Write(senha + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void ReiniciarFila()
+        {
+            _filaDeAtendimento.Clear();
+        }
+
+    }
+}
---
+++ FilaAtendimento/FilaAtendimento/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace FilaAtendimento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fila fila = new Fila();
+
+            fila.GerarSenha();
+            fila.GerarSenha();
+            fila.GerarSenha();
+
+            fila.ChamarProximo();
+
+            fila.MostrarFila();
+
+            fila.ReiniciarFila();
+
+            fila.MostrarFila();
+        }
+    }
+}
